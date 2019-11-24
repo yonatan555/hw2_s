a3fa0a0438e6d33194ffeb96412ac7ee95f05064
yonatan555 <57701623+yonatan555@users.noreply.github.com>
2019-11-24 18:09:49 +0200

    Delete myBank.h

--- myBank.h
+++
@@ -1,8 +0,0 @@
-void openAccount(double intialSum);
-void balance(int accountNumber);
-void transfer(int accountNumber ,double sum);
-void pull(int accountNumber ,double sum);
-void closeAccount(int accountNumber);
-void addDependPercent(double percent);
-void printall();
-void closeAllAccounts();
